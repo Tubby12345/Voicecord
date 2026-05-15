1b4ac59c5be24a5c770e6fab8c4b6c550749e565
Tubby <147832014+Tubby12345@users.noreply.github.com>
2026-05-16 03:29:19 +0700

    Update main.py

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ async def heartbeat(ws, interval):
 async def main():
     uri = "wss://gateway.discord.gg/?v=10&encoding=json"
 
-    async with websockets.connect(uri, max_size=10 * 1024 * 1024) as ws:
+    async with websockets.connect(uri, max_size=None) as ws:
         hello = json.loads(await ws.recv())
         heartbeat_interval = hello["d"]["heartbeat_interval"]
 
